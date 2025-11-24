feca26a4ffb6fcb8a903e11a9765a230374aa057
Swayam Jethi <swayam.jethi@gmail.com>
2025-11-24 23:28:09 +0530

    Cleaning Welcome and layout

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -18,26 +18,7 @@ export default async function Layout({ children }: LayoutProps) {
           href="https://livekit.io"
           className="scale-100 transition-transform duration-300 hover:scale-110"
         >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={logo} alt={`${companyName} Logo`} className="block size-6 dark:hidden" />
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={logoDark ?? logo}
-            alt={`${companyName} Logo`}
-            className="hidden size-6 dark:block"
-          />
         </a>
-        <span className="text-foreground font-mono text-xs font-bold tracking-wider uppercase">
-          Built with{' '}
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://docs.livekit.io/agents"
-            className="underline underline-offset-4"
-          >
-            LiveKit Agents
-          </a>
-        </span>
       </header>
 
       {children}
